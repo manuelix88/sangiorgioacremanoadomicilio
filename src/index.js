76ec3589fd38dc7bab4a90176a01ea65b0a60e66
emanuele.aprea <emanuele.aprea@gmail.com>
2020-04-20 10:12:57 +0200

    img modifica

--- src/index.js
+++ src/index.js
@@ -74,10 +74,8 @@ export default class App extends Component {
 						}
 					</nav>
 					<h1 class="font-sans text-4xl md:text-5xl lg:text-6xl pt-10 text-gray-800 text-center capitalize">
-						<span class="block sm:inline-block" role="img" aria-label="biker">
-							🚴
-						</span>
-						{`${process.env.PREACT_APP_CITY} a Domicilio`}
+                  <img style="max-width: 678px; max-height: 460px; display: block; margin-left: auto; margin-right: auto;" src="./assets/images/san-giorgio-a-domicilio.png" alt="San Giorgio a Cremano a domicilio" />
+					
 					</h1>
 					<Router onChange={this.handleRoute}>
 						<Home path="/" results={results} />
